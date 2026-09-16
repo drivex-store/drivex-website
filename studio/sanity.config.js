c919ea4c632c3539a3bf5bc7df7255526a79cde2
drivex studio <328487159+drivex-store@users.noreply.github.com>
2026-09-16 14:55:51 +0630

    Update sanity.config.js

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { muxInput } from "sanity-plugin-mux-input";
-import { schemaTypes } from "./src/schemaTypes";
+import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
   name: 'default',
